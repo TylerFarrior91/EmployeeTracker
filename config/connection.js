a703ffc4e5f4118e166ac822601525bdb6cfca85
Tyler Farrior <tylerfarrior@gmail.com>
2023-09-18 16:53:15 -0400

    setup connection

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+require("dotenv").config()
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME
+});
+
+module.exports = db;
