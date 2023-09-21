a5ce5df0b2a8a630f1f524d9d79227c2e9e85820
Tyler Farrior <tylerfarrior@gmail.com>
2023-09-21 19:19:30 -0400

    adding more to index.js file.

--- index.js
+++ index.js
@@ -9,6 +9,49 @@ const viewAllEmployees = () => {
         start()
     })
 }
+const viewAllDepartments = () => {
+    db.query("SELECT * FROM department", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        start()
+    })
+}
+const viewAllRoles = () => {
+    db.query("SELECT * FROM role", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        start()
+    })
+}
+const addEmployee = () => {
+    db.query("INSERT * FROM employee", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        start()
+    })
+}
+const addDepartment = () => {
+    db.query("INSERT * FROM department", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        start()
+    })
+}
+const addRole = () => {
+    db.query("INSERT * FROM role", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        start()
+    })
+}
+const updateEmployeeRole = () => {
+    db.query("UPDATE employee SET? WHERE?", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        start()
+    })
+}
+
 
 const start = () => {
     inquirer.prompt([
@@ -24,7 +67,7 @@ const start = () => {
                 "Update Employee Role",
                 "view All Departments",
                 "view All Roles",
-             ]
+            ]
         }
     ]).then((res) => {
         if (res.action === "View All Employees") {
@@ -37,13 +80,14 @@ const start = () => {
             addRole();
         } else if (res.action === "Update Employee Role") {
             updateEmployeeRole();
-         } else if (res.action === "view All Departments") {
+        } else if (res.action === "view All Departments") {
             viewAllDepartments();
-            } else if (res.action === "view All Roles") {
-                viewAllRoles();
-            } else if (res.action === "Exit") {
-                console.log("Goodbye!");
-                process.exit();
-            }
+        } else if (res.action === "view All Roles") {
+            viewAllRoles();
+        } else if (res.action === "Exit") {
+            console.log("Goodbye!");
+            process.exit();
+        }
     });
 }
+start();
