4f0698b97f32abdfc66a14683c3f109fe85f3d2e
Tyler Farrior <tylerfarrior@gmail.com>
2023-09-21 18:15:21 -0400

    updating java script

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const db = require("./config/connection")
 const inquirer = require("inquirer");
 
-db.connect(function(err) {
-    if(err) throw err;
-    console.log("MYSQL CONNECTED")
-    start();
-});
+
+const viewAllEmployees = () => {
+    db.query("SELECT * FROM employee", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        start()
+    })
+}
 
 const start = () => {
     inquirer.prompt([
@@ -14,20 +17,33 @@ const start = () => {
             name: "action",
             message: "What would you like to do?",
             choices: [
-                "View All Employees"]
+                "View All Employees",
+                "Add Employee",
+                "Add Department",
+                "Add Role",
+                "Update Employee Role",
+                "view All Departments",
+                "view All Roles",
+             ]
         }
     ]).then((res) => {
-        if(res.action === "View All Employees") {
+        if (res.action === "View All Employees") {
             viewAllEmployees();
-        }
-    })
+        } else if (res.action === "Add Employee") {
+            addEmployee();
+        } else if (res.action === "Add Department") {
+            addDepartment();
+        } else if (res.action === "Add Role") {
+            addRole();
+        } else if (res.action === "Update Employee Role") {
+            updateEmployeeRole();
+         } else if (res.action === "view All Departments") {
+            viewAllDepartments();
+            } else if (res.action === "view All Roles") {
+                viewAllRoles();
+            } else if (res.action === "Exit") {
+                console.log("Goodbye!");
+                process.exit();
+            }
+    });
 }
-
-const viewAllEmployees = () => {
-    db.query("SELECT * FROM employee", function(err, res) {
-        if(err) throw err;
-        console.table(res);
-        start()
-    })
-}
-
